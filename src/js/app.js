4e675654b0e54a9f482628ddf494928effd952ea
Vladimir Zhukov <zhukovvlad@yandex.ru>
2019-12-26 00:01:19 +0300

    Initial commit

---
+++ src/js/app.js
@@ -0,0 +1,6 @@
+// TODO: write your code here
+import Character from './character';
+
+const user = new Character('Zhukovvlad', 'Bowman');
+
+console.log(user);
---
+++ src/js/character.js
@@ -0,0 +1,12 @@
+/**
+ * <Общее описание>
+ *
+ * @param <описание параметра>
+ * @param <описание параметра>
+ *
+ * @throws <описание генерируемой ошибки>
+ */
+export default function Character(name, type) {
+  this.name = name;
+  this.type = type;
+}
